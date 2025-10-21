d5e3e2cc644c1931a088131acb839b7c4a0ca7b6
David FG <david.026697@gmail.com>
2025-10-21 20:56:58 -0300

    seccion de videos del canal y stylos a componente

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../../services/youtube.service';
+import { Video } from '../../models/video';
 
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.scss']
 })
-export class VideosComponent {
+export class VideosComponent implements OnInit {
+  videos: Video[] = [];
+  loading = true;
 
+  constructor(private youtubeService: YoutubeService) { }
+
+  ngOnInit(): void {
+    this.youtubeService.getVideos().subscribe({
+      next: (data) => {
+        this.videos = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.loading = false;
+      }
+    });
+  }
 }
