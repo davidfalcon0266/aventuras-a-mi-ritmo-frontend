4dd777eee8fd8b6051bd7719065139277a8fc4f7
David FG <david.026697@gmail.com>
2025-10-20 22:19:52 -0300

    Se crea la estructura del proyecto, forntend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { VideoGridComponent } from './components/video-grid/video-grid.component';
+import { VideoPlayerComponent } from './components/video-player/video-player.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { PlaylistsComponent } from './pages/playlists/playlists.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    VideoGridComponent,
+    VideoPlayerComponent,
+    AboutComponent,
+    HomeComponent,
+    VideosComponent,
+    PlaylistsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/video-grid/video-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-grid',
+  templateUrl: './video-grid.component.html',
+  styleUrls: ['./video-grid.component.scss']
+})
+export class VideoGridComponent {
+
+}
